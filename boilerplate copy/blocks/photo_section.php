1cf1a7450419c5475b64dd0b6fc3159cdacf8adf
marcobrunettionqor <marco.brunetti@onqor.co.uk>
2023-09-15 18:17:23 +0100

    homepage changes

--- boilerplate copy/blocks/photo_section.php
+++ boilerplate copy/blocks/photo_section.php
@@ -3,7 +3,9 @@
 ?>
 
 <div class="photo-section" alt="<?php echo($image["alt"]) ?>" style='background-image: url("<?php echo($image["sizes"]["onqor-xl"]) ?>")'>
-      <div class="photo-section__text">
-           <?php echo($text) ?>
+      <div class="photo-section__container">
+            <div class="photo-section__text">
+                  <?php echo($text) ?>
+            </div>
       </div>
 </div>
