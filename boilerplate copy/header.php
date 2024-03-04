2b7813bba185cde2ce963f968bfee72c7909a483
D'André Phillips <dandre.phillips@onqor.co.uk>
2024-03-04 16:30:33 +0000

    Added Hotjar code

--- boilerplate copy/header.php
+++ boilerplate copy/header.php
@@ -37,6 +37,19 @@
 	<?php // end of wordpress head ?>
 
 	<?php // drop Google Analytics Here ?>
+
+	<!-- Hotjar Tracking Code for Abbington House -->
+	<script>
+		(function(h,o,t,j,a,r){
+			h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+			h._hjSettings={hjid:3605603,hjsv:6};
+			a=o.getElementsByTagName('head')[0];
+			r=o.createElement('script');r.async=1;
+			r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+			a.appendChild(r);
+		})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+	</script>
+
 	<?php // end analytics ?>
 
 </head>
